f7c5ee4d6cc0b1b24fad53e7d16cf6e641e341ad
Anh Phuc Nguyen <anhphucnguyen311@gmail.com>
2020-06-20 17:40:15 +0700

    ok

--- server.js
+++ server.js
@@ -32,4 +32,4 @@ app.use("/api", userRoute)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log('Server started on port 4000'))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
